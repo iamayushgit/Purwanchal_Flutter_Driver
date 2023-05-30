9dd71ecbb1c22b26fddbd68bfd60544f90e7bf5b
ITsoch <sarojenvato@gmail.com>
2023-05-30 11:49:30 +0545

    first commit

---
+++ lib/theme/dark_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+ThemeData dark = ThemeData(
+  fontFamily: 'Roboto',
+  // primaryColor: Color(0xFFffbd5c),
+  primaryColor: Color(0xFF374BA0),
+
+  secondaryHeaderColor: Color(0xFF009f67),
+  disabledColor: Color(0xFF6f7275),
+  errorColor: Color(0xFFdd3135),
+  brightness: Brightness.dark,
+  hintColor: Color(0xFFbebebe),
+  cardColor: Colors.black,
+  colorScheme: ColorScheme.dark(
+      primary: Color(0xFFffbd5c), secondary: Color(0xFFffbd5c)),
+  textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(primary: Color(0xFFffbd5c))),
+);
